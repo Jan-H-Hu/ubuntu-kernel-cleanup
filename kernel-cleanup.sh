5203ec2974ef44594766f92dfbdb6af484578cd3
Emanuel Hategan <emanuel.hategan@realvnc.com>
2017-04-25 11:58:44 +0100

    Ignore the hardware enablement stack

--- kernel-cleanup.sh
+++ kernel-cleanup.sh
@@ -6,7 +6,7 @@
 KERNEL_RELEASE=`uname -r`
 echo "current kernel release $KERNEL_RELEASE"
 
-PROPOSED_FOR_REMOVAL=`dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | grep -v "linux-libc-dev"`
+PROPOSED_FOR_REMOVAL=`dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | grep -v -E "linux-libc-dev|-hwe-"`
 
 if [ -n "$PROPOSED_FOR_REMOVAL" ]
 then
